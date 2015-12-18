df1261e5418e7df48c6cf79d9c321e35e3e95fdc
kobake <kobake@users.sourceforge.net>
2015-12-19 01:18:33 +0900

    管理者権限を自動的に要求するシンボリックリンク作成コマンド

---
+++ vc10.0/lnhdrlink/Logger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lnhdrlink
+{
+	class Logger
+	{
+		public static void Log(string msg)
+		{
+			Console.WriteLine(msg);
+		}
+	}
+}
